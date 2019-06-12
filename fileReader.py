f82955a6f2fd1f725d594a5ce4f29efa8a31941d
Juan Rodrigo Gonzalez Fernandez <juan.rodrigo.gonzalez.fernandez@cern.ch>
2019-06-13 00:41:23 +0200

    Adding new launcher

---
+++ fileReader.py
@@ -0,0 +1,249 @@
+import os, sys, argparse
+from ROOT import TFile, TTree, TH1F
+
+defaultPath = '/afs/cern.ch/work/j/jrgonzal/public/Run2017G/skim2l'
+
+def isdigit(a):
+  ''' Redefinition of str.isdigit() that takes into account negative numbers '''
+  if a.isdigit(): return True
+  m = a[0]; n = a[1:]
+  if m == '-' and n.isdigit(): return True
+  return False
+
+def findValidRootfiles(path, sampleName = '', getOnlyNumberedFiles = False, verbose = False, FullPaths = False):
+  ''' Find rootfiles in path with a given name '''
+  files = []
+  if ',' in sampleName:
+    sl = sampleName.replace(' ', '').split(',')
+    return findValidRootfiles(path, sl, getOnlyNumberedFiles, verbose, FullPaths)
+  elif isinstance(sampleName, list):
+    for s in sampleName:
+      files += findValidRootfiles(path, s, getOnlyNumberedFiles, verbose, FullPaths)
+    return files
+  if not path[-1] == '/': path += '/'
+  if verbose: print ' >> Looking for files in path: ' + path
+  for f in os.listdir(path):
+    if not f[-5:] == '.root': continue
+    if not '_' in f: continue
+    n = f[:-5].split('_')[-1]
+    s = f[:-5].split('_')[:-1]
+    if not isdigit(n): s.append(n)
+    fname = ''
+    for e in s: fname+=e+'_'
+    if fname[-1] == '_': fname = fname[:-1]
+    if getOnlyNumberedFiles and not n.isdigit(): continue
+    if sampleName != '' and fname != sampleName and (fname+'_'+n) != sampleName: continue
+    if verbose: print ' >> Adding file: ', f
+    files.append(f)
+  if FullPaths: files = [path + x for x in files]
+  if len(files) == 0: print '[ERROR]: Not files "' + sampleName + '" found in: ' + path
+  return files
+
+def GetFiles(path, name, verbose = False):
+  ''' Get all rootfiles in path for a given process name'''
+  return findValidRootfiles(path, name, False, verbose, FullPaths = True)
+
+def GetNGenEvents(fname):
+  ''' Returns number of events from the 'Count' histograms '''
+  if isinstance(fname, list):
+    c = 0
+    for f in fname: c+=GetNGenEvents(f)
+    return c
+  elif isinstance(fname, str):
+    f = TFile.Open(fname)
+    h = f.Get('Count')
+    return h.GetBinContent(1)
+  else: print '[ERROR] [GetNGenEvents]: wrong input' 
+
+def GetSumWeights(fname):
+  ''' Returns number of events from the 'SumWeights' histograms '''
+  if isinstance(fname, list):
+    c = 0
+    for f in fname: c+=GetSumWeights(f)
+    return c
+  elif isinstance(fname, str):
+    f = TFile.Open(fname)
+    h = f.Get('SumWeights')
+    return h.GetBinContent(1)
+  else: print '[ERROR] [GetSumWeights]: wrong input' 
+
+def GetHistoFromSetOfFiles(fname, histoname):
+  ''' Returns the sum of a histo with a name in a list of files '''
+  if isinstance(fname, list):
+    h = GetHistoFromSetOfFiles(fname[0], histoname)
+    for f in fname[1:]: h.Add(GetHistoFromSetOfFiles(f, histoname))
+    return h
+  elif isinstance(fname, str):
+    f = TFile.Open(fname)
+    if not hasattr(f, histoname):
+      print '[ERROR] [GetHistoFromSetOfFiles] Histogram \'%s\' does not exist in file %s !!'%(hitoname, fnmae)
+    h = f.Get(histoname)
+    h.SetDirectory(0)
+    return h
+  else: print '[ERROR] [GetHistoFromSetOfFiles]: wrong input' 
+
+def GetEntries(fname):
+  ''' Returns number of events from the tree 'Events' in a file '''
+  if isinstance(fname, list):
+    c = 0
+    for f in fname: c+=GetEntries(f)
+    return c
+  elif isinstance(fname, str):
+    f = TFile.Open(fname)
+    t = f.Get('Events')
+    return t.GetEntries()
+  else: print '[ERROR] [GetEntries]: wrong input' 
+
+def GuessIsData(fname):
+  ''' Guess if a tree is data or simulation '''
+  if isinstance(fname, list): fname = fname[0] # Assume all files are the same process/dataset
+  f = TFile.Open(fname)
+  t = f.Get('Events')
+  if hasattr(t,'genWeight'): return False
+  return True
+
+def guessPathAndName(p):
+  ''' Guess path and sample name for a given rootfile '''
+  path = ''; n = -1
+  while '/' in p:
+    path += p[:p.index('/')+1]
+    p     = p[p.index('/')+1:]
+  if p[-5:] == '.root': p = p[:-5]
+  elif os.path.isdir(path + p):
+    path = path + p
+    p = ''
+  if len(path) > 0 and not path[-1] == '/': path += '/'
+  if '_' in p: 
+    n = p.split('_')[-1]
+    s = p.split('_')[:-1]
+    if not isdigit(n): 
+      s.append(n)
+      n = '-1'
+    p = ''
+    for e in s: p+=e+'_'
+    if p[-1] == '_': p = p[:-1]
+  return path, p, n
+
+def guessProcessName(fileName):
+  ''' Guess the name of the process for a given file '''
+  if isinstance(fileName, list): 
+    path, name, n = guessPathAndName(fileName[0])
+    fileName = name
+    if fileName[-5:] == '.root': fileName = fileName[:-5]
+    while '/' in fileName: fileName = fileName[fileName.index('/')+1:]
+  return fileName
+
+def groupFilesInDic(listOfFiles, name, i=-1, verbose = False):
+  ''' Manages a dictionary with sample names and lists of samples '''
+  if isinstance(name, list):
+    for e in name:
+      path, nam, n = guessPathAndName(e)  
+      groupFilesInDic(listOfFiles, nam, n)
+    return
+  fname = name + '_' + str(i) + '.root' if str(i).isdigit() else name + '.root'
+  if name in listOfFiles: listOfFiles[name].append(fname)
+  else: 
+    newList = [fname]
+    listOfFiles[name] = newList
+    if verbose: print ' >> Sample found: ' + name
+
+def getDicFiles(inFolder):
+  ''' Get a dictionary with sample names and lists of files  '''
+  listOfFiles = {}
+  files = findValidRootfiles(inFolder)
+  groupFilesInDic(listOfFiles,files)
+  return listOfFiles
+  
+def GetAllInfoFromFile(fname):
+  ''' Returns a list with all the info of a file ''' 
+  if isinstance(fname, list):
+    nEvents = 0
+    nGenEvents = 0
+    nSumOfWeights = 0
+    isData = False
+    for f in fname: 
+      iE, iG, iS, isData = GetAllInfoFromFile(f)
+      nEvents += iE
+      nGenEvents += iG
+      nSumOfWeights += iS
+    return [nEvents, nGenEvents, nSumOfWeights, isData]
+  elif isinstance(fname, str):
+    f = TFile.Open(fname)
+    t = f.Get('Events')
+    hs = f.Get('SumWeights')
+    hc = f.Get('Count')
+    nEvents = t.GetEntries()
+    nGenEvents = hc.GetBinContent(1) if isinstance(hc,TH1F) else 1
+    nSumOfWeights = hs.GetBinContent(1) if isinstance(hs,TH1F) else 1
+    isData = not hasattr(t,'genWeight')
+    return [nEvents, nGenEvents, nSumOfWeights, isData]
+  else: print '[ERROR] [GetAllInfoFromFile]: wrong input' 
+
+def GetProcessInfo(path, process=''):
+  ''' Prints all info from a process in path '''
+  if isinstance(path, list): 
+    files = path
+    path, process, k = guessPathAndName(files[0])
+  else: files = GetFiles(path, process)
+  nEvents, nGenEvents, nSumOfWeights, isData = GetAllInfoFromFile(files)
+  fileType = '(Data)' if isData else ('(MC)')
+  print '\n##################################################################'
+  print ' path: ' + path
+  print ' Process:            ' + process + ' ' + fileType
+  print ' Number of files:    ' + str(len(files))
+  print ' Total entries:      ' + str(nEvents)
+  if isData:
+    print ' Triggered events:   ' + str(nGenEvents)
+  else: 
+    print ' Generated events:   ' + str(nGenEvents)
+    print ' Sum of gen weights: ' + str(nSumOfWeights)
+  print '##################################################################\n'
+
+##################################
+# Extra functions to work check .root files from terminal
+
+def addToListOfFiles(listOfFiles, name, i):
+  ''' Manages a dictionary with sample names and lists of samples '''
+  fname = name + '_' + str(i) + '.root'
+  if name in listOfFiles: listOfFiles[name].append(fname)
+  else: 
+    newList = [fname]
+    listOfFiles[name] = newList
+    if verbose: print ' >> Sample found: ' + name
+
+def main():
+ # Parsing options
+ path = defaultPath
+ sample = ''
+ pr = argparse.ArgumentParser()
+ pr.add_argument('path', help='Input folder', type = str, default = defaultPath)
+ pr.add_argument('--sample', type = str, default = '')
+ pr.add_argument('-p','--inspect', action='store_true', help='Print branches')
+ args = pr.parse_args()
+ if args.sample:  sample = args.sample
+ printb = args.inspect
+ path = args.path
+ if os.path.isdir(path) and not path[-1] == '/': path += '/'
+
+ if sample == '':
+   origpath = path
+   path, sample, n = guessPathAndName(path)
+
+   if sample == '': 
+     d = getDicFiles(path)
+     for c in d:
+       print ' >> ' + c + ': ', d[c]
+
+   else:
+     totfile = path + sample + '_' + n + '.root' if int(n) >= 0 else path + sample + '.root'
+     if os.path.isfile(totfile): 
+       GetProcessInfo([totfile])
+       exit()
+     else:
+       GetProcessInfo(path, sample)
+ else:
+   GetProcessInfo(path, sample)
+   exit()
+
+if __name__ == '__main__':
+  main()
---
+++ run.py
@@ -0,0 +1,242 @@
+'''
+  Example: 
+  python run.py tt -p /pool/ciencias/userstorage/juanr/nanoAODv4/2017/ -s TTTo2L2Nu
+'''
+import os, sys
+from fileReader import getDicFiles, GetAllInfoFromFile
+
+def loadxsecdic(fname):
+  xsecdir = {}
+  if not os.path.isfile('./'+fname):
+    l = filter(lambda x: x[0] == fname, [x.split('.') for x in os.listdir('.')])
+    if len(l) == 0:
+      print 'ERROR: not found file %s with cross sections...'
+      return
+    else: l = l[0]
+    fname = l[0] + '.' + l[1]
+  print ' >> Reading cross section from %s...'%fname
+  f = open(fname)
+  lines = f.readlines()
+  for l in lines:
+    l = l.replace(' ', '')
+    l = l.replace('\n', '')
+    if l.startswith('#'): continue
+    if not ':' in l: continue
+    if '#' in l: l = l.split('#')[0]
+    if l == '': continue
+    lst = l.split(':')
+    key = lst[0]
+    val = lst[1]
+    if val == '': val = 1
+    xsecdir[key] = float(val)
+  return xsecdir 
+    
+def RunSamplePAF(selection, path, sample, year = 2018, xsec = 1, nSlots = 1, outname = '', outpath = '', nEvents = 0, FirstEvent = 0, prefix = 'Tree', verbose = True, pretend = False, dotest = False):
+
+  if ',' in sample:
+    sample.replace(' ', '')
+    sample = sample.split(',')
+  if not isinstance(sample, list): sample = [sample]
+  
+  if not path.endswith('/'): path += '/'
+  
+  if isinstance(xsec, int): xsec = float(xsec)
+  if isinstance(xsec, str): 
+    xsecdic = loadxsecdic(xsec)
+    s = sample[0]
+    if not s in xsecdic.keys():
+      print 'ERROR: not found xsec value for sample %s'%s
+      xsec = 1
+    else: xsec = xsecdic[s]
+  
+  # Get dictionary with all files in the path directory
+  dic = getDicFiles(path)
+  nfileInPath = len(dic)
+  if verbose: print 'Found %i files in path %s'%(nfileInPath, path)
+  
+  samples = []
+  for s in sample: 
+    dk = dic.keys()
+    if not s in dk: s = prefix+'_'+s
+    if not s in dk:
+      print 'WARNING: file %s not in path %s'%(s, path)
+    else:
+      samples += dic[s]
+  
+  nEventsInTree, nGenEvents, nSumOfWeights, isData = GetAllInfoFromFile([path + x for x in samples])
+  isamcatnlo = True if nGenEvents != nSumOfWeights else False
+  
+  stipe = 'MC' if not isData else 'DATA'
+  if verbose: 
+    print '## Procesing a %i %s sample...'%(year, stipe)
+    print '## Files found in %s'%path
+    for f in samples: print ' >> %s'%f
+    print '## Total events:      %i'%nEventsInTree
+    print '## Generated events:  %i'%nGenEvents
+    if isamcatnlo:
+      print '## Sum of weights:    %1.2f'%nSumOfWeights
+      print '## This sample has gen weights!!'
+  
+  if pretend: exit()
+  
+  from ROOT import PAFProject, PAFIExecutionEnvironment ,PAFSequentialEnvironment, PAFPROOFLiteEnvironment, PAFPoDEnvironment
+  from ROOT import vector, TString, gSystem
+  
+  # PAF mode selection (based on number of slots)
+  pafmode = PAFSequentialEnvironment();
+  if   nSlots <=  1: pafmode = PAFSequentialEnvironment();
+  elif nSlots <= 64: pafmode = PAFPROOFLiteEnvironment(nSlots);
+  else             : pafmode = PAFPoDEnvironment(nSlots);
+  
+  myProject = PAFProject(pafmode); 
+  
+  # Add the input data files
+  v = vector(TString)()
+  for s in samples: 
+    t = TString(s)
+    v.push_back(t)
+  myProject.AddDataFiles(v); 
+  myProject.SetDefaultTreeName("Events");
+  
+  # Deal with first and last event
+  if nEvents > 0    : myProject.SetNEvents(nEvents);
+  if FirstEvent > 0 : myProject.SetFirstEvent(FirstEvent);
+  
+  # Set output file
+  myProject.SetOutputFile(outpath + "/" + outname + ".root");
+  
+  # outpath
+  if outpath == '': outpath = "./"+selection+"_temp"
+  gSystem.mkdir(outpath, 1);
+  
+  # Parameters for the analysis
+  myProject.SetInputParam("sampleName",        outname);
+  myProject.SetInputParam("IsData",            isData    );
+  myProject.SetInputParam("weight",            xsec/nSumOfWeights);
+  myProject.SetInputParam("IsMCatNLO",         isamcatnlo);
+  myProject.SetInputParam("iselection",        selection);
+  myProject.SetInputParam("WorkingDir",        os.getcwd());
+  myProject.SetInputParam("nEntries",          nEventsInTree);
+  myProject.SetInputParam("xsec",              xsec);
+  myProject.SetInputParam("_options",          options);
+  myProject.SetInputParam("Year",              year);
+  
+  # Name of analysis class
+  myProject.AddSelectorPackage("LeptonSelector");
+  myProject.AddSelectorPackage("JetSelector");
+  myProject.AddSelectorPackage("EventBuilder");
+  
+  # Analysis selector
+  if      (selection == "tt"   or selection == "ttbar"):   myProject.AddSelectorPackage("TopAnalysis");
+  elif    (selection == "tWtt" or selection == "TWTT") :   myProject.AddSelectorPackage("TWTTbarAnalysis");
+  else:   print "UNKNOWN SELECTOR."
+  
+  # Additional packages
+  myProject.AddPackage("Lepton");
+  myProject.AddPackage("Jet");
+  myProject.AddPackage("mt2");
+  myProject.AddPackage("Functions");
+  myProject.AddPackage("LeptonSF");
+  myProject.AddPackage("BTagSFUtil");
+  #myProject.AddPackage("PUWeight");
+  
+  myProject.Run();
+
+  
+################################################################################
+### Execute
+################################################################################
+import argparse
+parser = argparse.ArgumentParser(description='Run with PAF')
+parser.add_argument('--verbose','-v'    , action='store_true'  , help = 'Activate the verbosing')
+parser.add_argument('--pretend'         , action='store_true'  , help = 'Create the files but not send the jobs')
+parser.add_argument('--test','-t'       , action='store_true'  , help = 'Sends only one or two jobs, as a test')
+parser.add_argument('selection'                                , help = 'Name of the selector')
+parser.add_argument('--path','-p'       , default=''           , help = 'Path to look for nanoAOD')
+parser.add_argument('--sample','-s'     , default=''           , help = 'Sample(s) to process')
+parser.add_argument('--xsec','-x'       , default='xsec'       , help = 'Cross section')
+parser.add_argument('--year','-y'       , default=2018         , help = 'Year')
+parser.add_argument('--conf','-c'       , default=''           , help = 'Config file (not yet implemented')
+parser.add_argument('--options','-o'    , default=''           , help = 'Options to pass to your analysis')
+parser.add_argument('--analysis','-a'   , default='Top'        , help = 'Analysis name')
+parser.add_argument('--prefix'          , default='Tree'       , help = 'Prefix of the name...')
+parser.add_argument('--outname'         , default=''           , help = 'Name of the output file')
+parser.add_argument('--outpath'         , default=''           , help = 'Output path')
+parser.add_argument('--firstEvent'      , default=0            , help = 'First event')
+parser.add_argument('--nEvents'         , default=0            , help = 'Number of events')
+parser.add_argument('--nSlots','-n'     , default=1            , help = 'Number of slots')
+
+args = parser.parse_args()
+
+verbose     = args.verbose
+pretend     = args.pretend
+dotest      = args.test
+sample      = args.sample
+path        = args.path
+options     = args.options
+xsec        = args.xsec
+year        = args.year
+selection   = args.selection
+prefix      = args.prefix
+outname     = args.outname
+outpath     = args.outpath
+nEvents     = args.nEvents
+nSlots      = args.nSlots
+FirstEvent  = args.firstEvent
+
+# Check if a cfg file is given as first argument
+fname = selection
+if not os.path.isfile('./'+fname):
+  l = filter(lambda x: x[0] == fname, [x.split('.') for x in os.listdir('.')])
+  if len(l) != 0: 
+    l = l[0]
+    fname = l[0] + '.' + l[1]
+if os.path.isfile(fname):
+  print ' >> Using config file \'%s\'...'%fname
+  spl = []
+  samplefiles = {}
+  f = open(fname)
+  lines = f.readlines()
+  for l in lines:
+    l = l.replace(' ', '')
+    l = l.replace('\n', '')
+    if l.startswith('#'): continue
+    if '#' in l: l = l.split('#')[0]
+    if l == '': continue
+    if not ':' in l: 
+      if   l == 'verbose': verbose = 1
+      elif l == 'pretend': pretend = 1
+      elif l == 'test'   : dotest  = 1
+      else:
+        spl.append(l)
+        samplefiles[l]=l
+    else:
+      lst = l.split(':')
+      key = lst[0]
+      val = lst[1]
+      if   key == 'pretend'   : pretend   = 1
+      elif key == 'verbose'   : verbose   = 1
+      elif key == 'test'      : dotest    = 1
+      elif key == 'path'      : path      = val
+      elif key == 'sample'    : sample    = val
+      elif key == 'options'   : options   = val
+      elif key == 'selection' : selection = val
+      elif key == 'xsec'      : xsec      = val
+      elif key == 'prefix'    : prefix    = val
+      elif key == 'outpath'   : outpath   = val
+      elif key == 'year'      : year      = int(val)
+      elif key == 'nSlots'    : nSlots    = int(val)
+      elif key == 'nEvents'   : nEvents   = int(val)
+      elif key == 'firstEvent': FirstEvent= int(val)
+      else:
+        spl.append(key)
+        samplefiles[key] = val
+
+  for sname in spl:
+    outname = sname
+    sample  = samplefiles[sname]
+    RunSamplePAF(selection, path, sample, year, xsec, nSlots, outname, outpath, nEvents, FirstEvent, prefix, verbose, pretend, dotest)
+
+else: # no config file...
+  RunSamplePAF(selection, path, sample, year, xsec, nSlots, outname, outpath, nEvents, FirstEvent, prefix, verbose, pretend, dotest)
+ 
