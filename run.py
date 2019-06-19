11603df96b1e063738d2936e3bb5321a2b33a7b8
Juan Rodrigo Gonzalez Fernandez <juan.rodrigo.gonzalez.fernandez@cern.ch>
2019-06-19 16:42:22 +0200

    Adding new options to run.py

--- run.py
+++ run.py
@@ -60,7 +60,8 @@ def loadxsecdic(fname, verbose):
     xsecdir[key] = float(val)
   return xsecdir 
 
-def GetXsec(xsec, s, verbose):
+def GetXsec(xsec, s, verbose, isdata):
+  if isdata: return 1
   if isinstance(xsec, int): xsec = float(xsec)
   if isinstance(xsec, str):
     xsecdic = loadxsecdic(xsec, verbose)
@@ -115,15 +116,17 @@ def RunSamplePAF(selection, path, sample, year = 2018, xsec = 1, nSlots = 1, out
   
   if not path.endswith('/'): path += '/'
   
-  xsec = GetXsec(xsec, outname, verbose)
   
   # Get dictionary with all files in the path directory
   samples = GetSampleList(path, sample)
   
   nEventsInTree, nGenEvents, nSumOfWeights, isData = GetAllInfoFromFile([path + x for x in samples])
+  xsec = GetXsec(xsec, outname, verbose, isData) if not dotest else 1
   isamcatnlo = True if nGenEvents != nSumOfWeights else False
-  stipe = 'MC' if not isData else 'DATA'
+  if isData: isamcatnlo = False
+
   if verbose: 
+    stipe = 'MC' if not isData else 'DATA'
     print '## Processing a %i %s sample...'%(year, stipe)
     print '## Files found in %s'%path
     for f in samples: print ' >> %s'%f
@@ -261,7 +264,7 @@ parser.add_argument('--outpath'         , default=''           , help = 'Output 
 parser.add_argument('--queue'           , default='short'      , help = 'Queue to send jobs')
 parser.add_argument('--firstEvent'      , default=0            , help = 'First event')
 parser.add_argument('--nEvents'         , default=0            , help = 'Number of events')
-parser.add_argument('--nSlots','-n'     , default=1            , help = 'Number of slots')
+parser.add_argument('--nSlots','-n'     , default=-1           , help = 'Number of slots')
 
 args = parser.parse_args()
 aarg = sys.argv
@@ -345,7 +348,6 @@ if os.path.isfile(fname):
   if '--pretend' in aarg or '-p' in aarg : pretend     = args.pretend
   if '--test'    in aarg or '-t' in aarg : dotest      = args.test
   if '--sendJobs'in aarg or '-j' in aarg : sendJobs    = args.sendJobs
-  if args.sample     != ''     : sample      = args.sample
   if args.path       != ''     : path        = args.path
   if args.options    != ''     : options     = args.options
   if args.xsec       != 'xsec' : xsec        = args.xsec
@@ -354,9 +356,30 @@ if os.path.isfile(fname):
   if args.outname    != ''     : outname     = args.outname
   if args.outpath    != ''     : outpath     = args.outpath
   if args.nEvents    != 0      : nEvents     = args.nEvents
-  if args.nSlots     != 1      : nSlots      = args.nSlots
   if args.firstEvent != 0      : FirstEvent  = args.firstEvent
   if args.queue      != 0      : queue       = args.queue
+
+  if args.nSlots     != -1: 
+    nSlots      = int(args.nSlots)
+    for k in nslots.keys(): nslots[k] = nSlots
+  elif nSlots == -1: 
+    nSlots = 1
+
+  if args.sample     != '': 
+    sample = args.sample
+    if not sample in samplefiles.keys():
+      print 'WARNING: Sample \'%s\' not found in cfg file!!'%sample
+      samplefiles[sample] = sample
+      nslots[sample] = nSlots
+    spl = [sample]
+
+  if dotest:
+    nEvents = 1000
+    queue = 'cpupower'
+    spl = spl[0:1]
+    nslots[spl[0]] = 1
+    #samplefiles[spl[0]] = [samplefiles[spl[0]][0]]
+    outname = 'test'
 
   for sname in spl:
     outname = sname
