5bb66520e8ab22c7b41f4141c9f044e733c8cd41
Juan Rodrigo Gonzalez Fernandez <juan.rodrigo.gonzalez.fernandez@cern.ch>
2019-10-03 12:22:33 +0200

    Adding path as parameter

--- run.C
+++ run.C
@@ -65,6 +65,7 @@ void run(TString samp, TString selection, Double_t xsec, Double_t sumofweights, 
   myProject->SetInputParam("selection",  selection);
   myProject->SetInputParam("WorkingDir", workingdir);
   myProject->SetInputParam("xsec",       xsec);
+  myProject->SetInputParam("path",       path);
   myProject->SetInputParam("_options",   options);
   myProject->SetInputParam("year",       TString(Form("%i",year)));
   
@@ -80,6 +81,7 @@ void run(TString samp, TString selection, Double_t xsec, Double_t sumofweights, 
     myProject->AddSelectorPackage("LepEffTop");
   
   // Additional packages
+  myProject->AddPackage("SUSYnorm");
   myProject->AddPackage("Lepton");
   myProject->AddPackage("Jet");
   myProject->AddPackage("mt2");
